4c54e9b053ecc61bba79a9f9e54904084bd830c5
Amara Jaeger <abj2126@barnard.edu>
2019-11-18 18:00:42 -0500

    fixed motion graphics

--- rotate-scale-along-path.js
+++ rotate-scale-along-path.js
@@ -1,7 +1,7 @@
 var svgWidth = 800
 var svgHeight = 800
 
-var rotateAngle = 20
+var rotateAngle = 45
 
 var svg = d3.select("#graph").append("svg")
     .attr("width", svgWidth)
@@ -57,11 +57,16 @@ function createNodes(numNodes, radius) {//http://bl.ocks.org/bycoffe/3404776
         y,
         i;
     for (i = 0; i < numNodes + 1; i++) {
-        angle = (i / (numNodes / 2)) * Math.PI - 1.5; // Calculate the angle at which the element will be placed.
+        angle = (i / (numNodes / 2)) * Math.PI - Math.PI/2; // Calculate the angle at which the element will be placed.
         console.log(angle)                // For a semicircle, we would use (i / numNodes) * Math.PI.
         x = (radius * Math.cos(angle)) + (width / 2); // Calculate the x position of the element.
         y = (radius * Math.sin(angle)) + (width / 2); // Calculate the y position of the element.
         nodes.push([x, y]);
+        svg.append("circle")
+        .attr("cx", x)
+        .attr("cy", y)
+        .attr("fill", "purple")
+        .attr("r", 3)
     }
     return nodes;
 }
@@ -79,14 +84,15 @@ var path = svg.append("path")
 
 
 //draw the little sticks for the plates left and right
-var stemHeight = 30
-var stemWidth = 2
-var plateHeight = 2
+var stemHeight = 60
+var stemWidth = 15
+var plateHeight = 4
+var plateWidth = 45
 
 var leftContainer = svg.append("g")
 var leftStem = leftContainer.append("rect")
     .attr("x", svgWidth / 4) //Starting x
-    .attr("y", svgWidth / 2 - stemHeight) //Starting y
+    .attr("y", svgHeight / 2 - stemHeight) //Starting y
     .attr("height", stemHeight)
     .attr("width", stemWidth)
     .attr("id", "leftStem")
@@ -96,10 +102,10 @@ var leftStem = leftContainer.append("rect")
     .duration(10000)
     .tween("pathTween", function () { return pathTweenLeft(path) })
 var leftPlate = leftContainer.append("rect")
-    .attr("x", svgWidth / 4 - stemHeight / 2) //Starting x
-    .attr("y", svgWidth / 2 - stemHeight) //Starting y
+    .attr("x", svgWidth / 4 - plateWidth / 2) //Starting x
+    .attr("y", svgHeight / 2 - stemHeight) //Starting y
     .attr("height", plateHeight)
-    .attr("width", stemHeight)
+    .attr("width", plateWidth)
     .attr("id", "leftPlate")
     .attr("fill", "blue")
     .transition()
@@ -126,10 +132,10 @@ var rightStem = rightContainer.append("rect")
 
 //!!! this is the horizontal line
 var rightPlate = rightContainer.append("rect")
-    .attr("x", svgWidth * 3 / 4 - stemHeight / 2) //Starting x- these will have to be updated in the tween function below for the right
+    .attr("x", svgWidth * 3 / 4 - plateWidth / 2) //Starting x- these will have to be updated in the tween function below for the right
     .attr("y", svgWidth / 2 - stemHeight) //Starting y- these will have to be updated in the tween function below for the right
     .attr("height", plateHeight)
-    .attr("width", stemHeight)
+    .attr("width", plateWidth)
     .attr("id", "rightPlate")
     .attr("fill", "red")
     .transition()
@@ -144,14 +150,20 @@ function pathTweenRight(path) {
     var r = d3.interpolate(length / 4, length / 4 + length * rotateAngle / 360); //Set up interpolation from 0 to the path length
     return function (t) {
         var point = path.node().getPointAtLength(r(t)); // Get the next point along the path
+        // svg.append("circle")
+        // .attr("r", 3)
+        // .attr("cx", point.x)
+        // .attr("cy", point.y)
+        // .attr("fill", "gold");
+
         d3.select("#rightStem") // !!!Select vertical line
             .attr("x", point.x) //!!! Set the x tween at each point, play around with this number till it doesn't jump
-            .attr("y", point.y - stemHeight * 1.5) //!!!! Set the y as well
+            .attr("y", point.y - stemHeight) //!!!! Set the y as well
 
         //this is the horizontal one
         d3.select("#rightPlate") //!!! Select the horizontal one i called it plate
-            .attr("x", point.x - stemHeight / 2) //!!! Set the x
-            .attr("y", point.y - stemHeight * 1.5) //!!! Set the y
+            .attr("x", point.x - plateWidth / 2) //!!! Set the x
+            .attr("y", point.y - stemHeight) //!!! Set the y
     }
 }
 
@@ -162,9 +174,9 @@ function pathTweenLeft(path) {
         var point = path.node().getPointAtLength(r(t)); // Get the next point along the path
         d3.select("#leftStem") // Select the circle
             .attr("x", point.x) // Set the cx
-            .attr("y", point.y) // Set the cy
+            .attr("y", point.y - stemHeight) // Set the cy
         d3.select("#leftPlate") // Select the circle
-            .attr("x", point.x - stemHeight / 2) // Set the cx
-            .attr("y", point.y) // Set the cy
+            .attr("x", point.x - plateWidth / 2) // Set the cx
+            .attr("y", point.y - stemHeight) // Set the cy
     }
 }
