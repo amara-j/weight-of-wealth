31b1493f8f840900cc40302b1ebd53bba92d3fa8
Amara Jaeger <abj2126@barnard.edu>
2019-11-18 21:20:40 -0500

    restructured, now interactive, moves on click.

--- rotate-scale-along-path.js
+++ rotate-scale-along-path.js
@@ -26,35 +26,115 @@ graphContainer.append("circle")
     .attr("r", svgWidth / 4)
     .attr("opacity", .1)
 
+// draw right arm of scale     
+rightArm = graphContainer.append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', svgWidth / 4)
+    .attr('height', 2)
+    .attr('id', 'rightArm')
+
+// draw left arm of scale
+leftArm = graphContainer.append('rect')
+    .attr('x', -svgWidth / 4)
+    .attr('y', 0)
+    .attr('width', svgWidth / 4)
+    .attr('height', 2)
+    .attr('id', 'leftArm')
+
+// create small svg container for stem and plate unit on left side
+var leftContainer = svg.append("g")
+// draw left stem and assign it a starting position
+var leftStem = leftContainer.append("rect")
+    .attr("x", svgWidth / 4 - stemWidth / 2) //Starting x
+    .attr("y", svgHeight / 2 - stemHeight + stemOffset) //Starting y
+    .attr("height", stemHeight)
+    .attr("width", stemWidth)
+    .attr("id", "leftStem")
+    .attr("fill", "black")
+// draw left plate and assign it a starting position
+var leftPlate = leftContainer.append("rect")
+    .attr("x", svgWidth / 4 - plateWidth / 2) //Starting x
+    .attr("y", svgHeight / 2 - stemHeight + stemOffset) //Starting y
+    .attr("height", plateHeight)
+    .attr("width", plateWidth)
+    .attr("id", "leftPlate")
+    .attr("fill", "black")
+
+// do the same thing for the other side:
+// create small svg container for stem+plate unit on right side
+var rightContainer = svg.append("g")
+// draw right stem and assign it a starting position
+var rightStem = rightContainer.append("rect")
+    .attr("x", svgWidth * 3 / 4 - stemWidth / 2)
+    .attr("y", svgWidth / 2 - stemHeight + stemOffset)
+    .attr("height", stemHeight)
+    .attr("width", stemWidth)
+    .attr("id", "rightStem")
+    .attr("fill", "black")
+// draw right plate and assign it a starting position
+var rightPlate = rightContainer.append("rect")
+    .attr("x", svgWidth * 3 / 4 - plateWidth / 2)
+    .attr("y", svgWidth / 2 - stemHeight + stemOffset)
+    .attr("height", plateHeight)
+    .attr("width", plateWidth)
+    .attr("id", "rightPlate")
+    .attr("fill", "black")
+
+
 //draw circle at center of scale
 graphContainer.append("circle")
     .attr("cx", 0)
     .attr("cy", 0)
     .attr("r", centerCircleRadius)
     .attr("opacity", 1)
+    // make mouseover of scale center interactive, so it feels like a button
+    .on("mouseover", function () {
+        d3.select(this).attr("fill", "red")
+    })
+    .on("mouseout", function () {
+        d3.select(this).attr("fill", "black");
+    })
+    // when scale center is clicked, turn scale by rotateAngle parameter
+    // can change this parameter above
+    .on("click", function () {
+        // rotate left arm of scale
+        d3.select("#leftArm")
+            .transition()
+            .duration(10000)
+            .attr("transform", "rotate(" + rotateAngle + " 0 0)");
 
-//draw 2 arms as separate, and rotate each of them  
-// draw first arm          
-firstArm = graphContainer.append('rect')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('width', svgWidth / 4)
-    .attr('height', 2)
-    .transition()
-    .delay(1000)
-    .duration(10000)
-    .attr("transform", "rotate(" + rotateAngle + " 0 0)")
+        // rotate right arm of scale
+        d3.select("#rightArm")
+            .transition()
+            .duration(10000)
+            .attr("transform", "rotate(" + rotateAngle + " 0 0)");
 
-// draw second arm
-secondArm = graphContainer.append('rect')
-    .attr('x', -svgWidth / 4)
-    .attr('y', 0)
-    .attr('width', svgWidth / 4)
-    .attr('height', 2)
-    .transition()
-    .delay(1000)
-    .duration(10000)
-    .attr("transform", "rotate(" + rotateAngle + " 0 0)")
+        d3.select("#leftStem")
+            .transition()
+            .duration(10000)
+            // call the tweening function to update new position
+            .tween("pathTween", function () { return pathTweenLeft(path) })
+
+        d3.select("#leftPlate")
+            .transition()
+            .duration(10000)
+            // call the tweening function to update new position
+            .tween("pathTween", function () { return pathTweenLeft(path) })
+
+        d3.select("#rightStem")
+            .transition()
+            .duration(10000)
+            // call the tweening function to update new position
+            .tween("pathTween", function () { return pathTweenRight(path) })
+
+        d3.select("#rightPlate")
+            .transition()
+            .duration(10000)
+            // call the tweening function to update new position
+            .tween("pathTween", function () { return pathTweenRight(path) })
+    })
+
 
 //generate a series of nodes around the circle
 // visualize them with circles to be sure it's working as it should
@@ -90,69 +170,6 @@ var path = svg.append("path")
     .attr("opacity", .3)
     .style("fill", "none");
 
-// create small svg container for stem and plate unit on left side
-var leftContainer = svg.append("g")
-// draw left stem and assign it a starting position
-var leftStem = leftContainer.append("rect")
-    .attr("x", svgWidth / 4 - stemWidth / 2) //Starting x
-    .attr("y", svgHeight / 2 - stemHeight + stemOffset) //Starting y
-    .attr("height", stemHeight)
-    .attr("width", stemWidth)
-    .attr("id", "leftStem")
-    .attr("fill", "purple")
-    .transition()
-    .delay(1000)
-    .duration(10000)
-    // call the tweening function to update new position
-    .tween("pathTween", function () { return pathTweenLeft(path) })
-
-// draw left plate and assign it a starting position
-var leftPlate = leftContainer.append("rect")
-    .attr("x", svgWidth / 4 - plateWidth / 2) //Starting x
-    .attr("y", svgHeight / 2 - stemHeight + stemOffset) //Starting y
-    .attr("height", plateHeight)
-    .attr("width", plateWidth)
-    .attr("id", "leftPlate")
-    .attr("fill", "blue")
-    .transition()
-    .delay(1000)
-    .duration(10000)
-    // call the tweening function to update new position
-    .tween("pathTween", function () { return pathTweenLeft(path) })
-
-// create small svg container for stem+plate unit on right side
-var rightContainer = svg.append("g")
-
-
-// draw right stem and assign it a starting position
-var rightStem = rightContainer.append("rect")
-    .attr("x", svgWidth * 3 / 4 - stemWidth / 2)
-    .attr("y", svgWidth / 2 - stemHeight + stemOffset)
-    .attr("height", stemHeight)
-    .attr("width", stemWidth)
-    .attr("id", "rightStem")
-    .attr("fill", "red")
-    .transition()
-    .delay(1000)
-    .duration(10000)
-    // call the tweening function to update new position
-    .tween("pathTween", function () { return pathTweenRight(path) })
-
-// draw right plate and assign it a starting position
-var rightPlate = rightContainer.append("rect")
-    .attr("x", svgWidth * 3 / 4 - plateWidth / 2)
-    .attr("y", svgWidth / 2 - stemHeight + stemOffset)
-    .attr("height", plateHeight)
-    .attr("width", plateWidth)
-    .attr("id", "rightPlate")
-    .attr("fill", "red")
-    .transition()
-    .delay(1000)
-    .duration(10000)
-    // call the tweening function to update new position
-    .tween("pathTween", function () { return pathTweenRight(path) })
-
-
 function pathTweenRight(path) {
     var length = path.node().getTotalLength();
     // Get the length of the path
@@ -161,13 +178,6 @@ function pathTweenRight(path) {
     return function (t) {
         var point = path.node().getPointAtLength(r(t));
         // Get the next point along the path
-        // visualize this with small circles at the new points
-        // so we can see where the path is to de-bug
-        svg.append("circle")
-            .attr("r", 3)
-            .attr("cx", point.x)
-            .attr("cy", point.y)
-            .attr("fill", "gold");
 
         d3.select("#rightStem")
             // select right stem
@@ -187,11 +197,6 @@ function pathTweenLeft(path) {
     var r = d3.interpolate(length * 3 / 4, length * 3 / 4 + length * rotateAngle / 360);
     return function (t) {
         var point = path.node().getPointAtLength(r(t));
-        svg.append("circle")
-            .attr("r", 3)
-            .attr("cx", point.x)
-            .attr("cy", point.y)
-            .attr("fill", "gold");
         d3.select("#leftStem")
             .attr("x", point.x - stemWidth / 2)
             .attr("y", point.y - stemHeight + stemOffset)
